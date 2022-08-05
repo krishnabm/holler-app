67ea0d26f182d08623485971e88196a8b279b65d
Krishna BM <krishnabmahadevan@gmail.com>
2022-08-05 21:43:43 +0530

    body handler

--- holler-core/src/main.rs
+++ holler-core/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![test_fetch])
     .menu(tauri::Menu::os_default(&context.package_info().name))
     .run(context)
     .expect("error while running tauri application");
@@ -15,4 +16,9 @@ fn main() {
 #[tauri::command]
 fn greet(name: &str) -> String {
    format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn test_fetch() -> String {
+   format!("I was populated asynchronously!")
 }
