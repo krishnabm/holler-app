789cbf2b9015ad1adaaa189edfb5956ea21016f1
Krishna BM <krishnabmahadevan@gmail.com>
2023-02-27 00:56:50 +0530

    config changes

--- holler-core/src/main.rs
+++ holler-core/src/main.rs
@@ -1,24 +1,24 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 fn main() {
-  let context = tauri::generate_context!();
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .invoke_handler(tauri::generate_handler![test_fetch])
-    .menu(tauri::Menu::os_default(&context.package_info().name))
-    .run(context)
-    .expect("error while running tauri application");
+    let context = tauri::generate_context!();
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![test_fetch])
+        .menu(tauri::Menu::os_default(&context.package_info().name))
+        .run(context)
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-  format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
 }
 
 #[tauri::command]
 fn test_fetch() -> String {
-  format!("I was populated asynchronously!")
+    format!("I was populated asynchronously!")
 }
