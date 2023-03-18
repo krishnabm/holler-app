74c9f7e3fad8a4f43f089dd97fefd633696dc038
Krishna BM <krishnabmahadevan@gmail.com>
2023-03-18 16:22:46 +0530

    Removed comments

--- holler-ui/src/components/RenderHoller/RenderHoller.tsx
+++ holler-ui/src/components/RenderHoller/RenderHoller.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { delay, from, interval, map, take } from 'rxjs';
 import { RenderProps } from '../../common/contracts/Props';
 
 export const RenderHoller = (props: RenderProps) => {
@@ -18,22 +17,6 @@ export const RenderHoller = (props: RenderProps) => {
       clearInterval(intervalId);
     };
   }, [renderSentence]);
-
-  // Approach 1 - setTimeOut
-  // for (let index = 0; index < wordList.length; index++) {
-  //   setTimeout(() => {
-  //     updateText(wordList[index]);
-  //   }, index * 1000);
-  // }
-
-  // Approach 2 - RxJS
-  // interval(1000).pipe(
-  //   take(wordList.length),
-  //   map(i => wordList[i])
-  // ).subscribe({
-  //   next: (word) => updateText(word),
-  //   error: _ => console.log('error'),
-  // });
 
   return (
     <div className="render-canvas">
